8c2099255febac0daa0d6824ad551aa99ca71df8
msm <1345241789@qq.com>
2017-01-10 23:37:52 +0800

    react

---
+++ index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+class HelloMessage extends React.Component {
+render() {
+return <div>Hello {this.props.name}</div>;
+}
+}
+
+ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
